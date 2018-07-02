2c78d1baf019dabe08e3c2ed4be8eea9dc1d2143
Ali Ilman <ali@ali-ilman.com>
2018-07-02 22:45:02 +0800

    Created about section.

--- src/javascript/ali.js
+++ src/javascript/ali.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav-container');
 const menu = document.querySelector('.menu');
 const navLinksContainer = document.querySelector('.nav-links-container');
+const navLinks = document.querySelectorAll('.nav-links-container')
+
+const aboutContainer = document.querySelector('.about-container');
 let isBodyBelowZero;
 
 // Helpers
@@ -8,6 +11,13 @@ const toggleAttribute = (elem, attr, one, two) => {
   const elemDataAttribute = elem.getAttribute(attr);
   elem.setAttribute(attr, elemDataAttribute === one ? two : one);
 };
+
+const scrollTo = elem => {
+  window.scroll({
+    behavior: 'smooth',
+    top: elem.offsetTop - 88,
+  });
+}
 
 // Events
 window.addEventListener('scroll', () => {
@@ -32,4 +42,8 @@ menu.addEventListener('click', () => {
 
 navLinksContainer.addEventListener('click', () => {
   nav.setAttribute('data-menu-opened', 'no');
+});
+
+document.getElementById("about").addEventListener('click', () => {
+  scrollTo(aboutContainer);
 });
