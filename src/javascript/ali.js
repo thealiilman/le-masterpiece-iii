8518268b00d72dc0fa5ab2892897e663bbdccbf0
Ali Ilman <ali@ali-ilman.com>
2018-07-15 16:39:22 +0800

    Created Contact section.

--- src/javascript/ali.js
+++ src/javascript/ali.js
@@ -5,11 +5,21 @@ const navLinks = document.querySelectorAll('.nav-links-container')
 
 const aboutLink = document.getElementById('about');
 const portfolioLink = document.getElementById('portfolio');
+const contactLink = document.getElementById('contact');
 
 const aboutContainer = document.querySelector('.about-container');
 const portfolioContainer = document.querySelector('.portfolio-container');
+const contactContainer = document.querySelector('.contact-container');
 
-let isBodyBelowZero;
+const sendButton = document.getElementById('formButton');
+const formMessage = document.querySelector('.form-message');
+
+const name = document.getElementById('formName');
+const email = document.getElementById('formEmail');
+const subject = document.getElementById('formSubject');
+const message = document.getElementById('formMessage');
+
+const NAVBAR_HEIGHT = 88;
 
 // Helpers
 const toggleAttribute = (elem, attr, one, two) => {
@@ -20,13 +30,13 @@ const toggleAttribute = (elem, attr, one, two) => {
 const scrollTo = elem => {
   window.scroll({
     behavior: 'smooth',
-    top: elem.offsetTop - 88,
+    top: elem.offsetTop - NAVBAR_HEIGHT,
   });
 }
 
 // Events
 window.addEventListener('scroll', () => {
-  isBodyBelowZero = document.body.getBoundingClientRect().top < 0;
+  const isBodyBelowZero = document.body.getBoundingClientRect().top < 0;
 
   if (isBodyBelowZero) {
     nav.setAttribute('data-position', 'below-zero');
@@ -49,12 +59,43 @@ navLinksContainer.addEventListener('click', () => {
   nav.setAttribute('data-menu-opened', 'no');
 });
 
-aboutLink.addEventListener('click', e => {
-  e.preventDefault();
-  scrollTo(aboutContainer);
-});
+aboutLink.addEventListener('click', () => scrollTo(aboutContainer));
 
-portfolioLink.addEventListener('click', e => {
+portfolioLink.addEventListener('click', () => scrollTo(portfolioContainer));
+
+contactLink.addEventListener('click', () => scrollTo(contactContainer));
+
+sendButton.addEventListener('click', e => {
   e.preventDefault();
-  scrollTo(portfolioContainer);
+
+  if (name.value === "" || email.value === "" || subject.value === "" || message.value === "") {
+    formMessage.setAttribute('data-message', 'error');
+    formMessage.textContent = "Please fill in the form.";
+  } else {
+    formMessage.textContent = "";
+
+    const url = 'http://ali-ilman.com/form.php';
+    const options = {
+      method: "POST",
+      body: new FormData(document.getElementById("contactForm"))
+    };
+
+    fetch(url, options)
+      .then(response => {
+        if (response.status === 200) {
+          formMessage.setAttribute('data-message', 'success');
+          formMessage.textContent = "Email has been sent! Ali will get back to you shortly.";
+        }
+      })
+      .catch(e => {
+        formMessage.setAttribute('data-message', 'error');
+        formMessage.textContent = "An error has occured.";
+        console.log(e);
+      });
+
+    name.value = "";
+    email.value = "";
+    subject.value = "";
+    message.value = "";
+  }
 });
